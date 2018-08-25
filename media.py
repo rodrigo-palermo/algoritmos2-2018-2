fd347eec2d397268a865a2bef6e61fdd820b88f0
Rodrigo Palermo <avilapalermo@gmail.com>
2018-08-25 01:25:38 -0300

    Start using English to code.

--- media.py
+++ media.py
@@ -1,28 +1,28 @@
-"""Exercicio aula 2."""
+"""Exercise 2."""
 
 num1 = 45
 num2 = 50
 num3 = 14
 
 
-def media(num1, num2):
-    """Calcula a média entre 2 números."""
+def mean(num1, num2):
+    """Calculates the mean between two numbres."""
     return (num1 + num2)/2
 
 
-media = media(num1, num2)
+mean = mean(num1, num2)
 
 
-def maiorDe3(num1, num2, num3):
-    """Calcula o maior valor entre 3 números."""
-    maior = num1
-    if num2 > maior:
-        maior = num2
-    elif num3 > maior:
-        maior = num3
-    return maior
+def biggest(num1, num2, num3):
+    """Computes the bigger value among three numbers."""
+    biggest = num1
+    if num2 > biggest:
+        biggest = num2
+    elif num3 > biggest:
+        biggest = num3
+    return biggest
 
 
-print("A média entre %.2f e %.2f é %.2f" % (num1, num2, media))
-print("Entre %.2f, %.2f e %.2f, o maior é: %.2f" % (num1, num2, num3,
-      maiorDe3(num1, num2, num3)))
+print("The mean between %.2f and %.2f is %.2f" % (num1, num2, mean))
+print("Among %.2f, %.2f e %.2f, the biggest is %.2f" % (num1, num2, num3,
+      biggest(num1, num2, num3)))

b6b5df894e2d6b221fed7363b9fdf82fe965fdf9
Rodrigo Palermo <avilapalermo@gmail.com>
2018-09-18 18:30:36 -0300

    Finished aula-05 BDD behave.

--- aula-05/features/steps/step_starfield.py
+++ aula-05/features/steps/step_starfield.py
@@ -23,7 +23,7 @@ def given_speed(context):
 def when_create_star(context):
     """When I create an object that represents a star."""
     context.star1 = create_star(x=800, y=[context.y_min, context.y_max],
-                                v=context.speed)
+                                s=context.speed)
 
 
 @then('its X coordinate is 800')
@@ -41,7 +41,7 @@ def then_y1(context):
 @then('its speed is 1, 2 or 3')
 def then_v1(context):
     """Then its speed is 1, 2 or 3."""
-    assert(context.star1['v'] in [1, 2, 3])
+    assert(context.star1['s'] in [1, 2, 3])
 
 
 # Scenario 2
@@ -51,10 +51,10 @@ def then_v1(context):
 def given_a_star(context):
     """Given an object reresenting a star."""
     context.star1 = create_star(x=800, y=[0, 600],
-                                v=[1, 2, 3])
+                                s=[1, 2, 3])
     context.x = context.star1['x']
     context.y = context.star1['y']
-    context.v = context.star1['v']
+    context.s = context.star1['s']
 
 
 @when('I move the star')
@@ -66,7 +66,7 @@ def when_moving(context):
 @then('its X coordinate changes according to its speed')
 def then_x2(context):
     """Then its X coordinate changes according to its speed."""
-    assert(context.star1['x'] == context.x + context.v)
+    assert(context.star1['x'] == context.x - context.s)
 
 
 @then('its Y coordinate does not change')
@@ -78,7 +78,7 @@ def then_y2(context):
 @then('its speed does not change')
 def then_v2(context):
     """Then its speed does not change."""
-    assert(context.star1['v'] == context.v)
+    assert(context.star1['s'] == context.s)
 
 
 # Scenario 3
@@ -87,29 +87,29 @@ def then_v2(context):
 @when('I create a list with 20 stars')
 def when_create_stars(context):
     """When I create a list with 20 stars."""
-    context.stars_list = create_stars_list(20, x=800, y=[0, 600], v=[1, 2, 3])
+    context.stars_list = create_stars_list(20, x=800, y=[0, 600], s=[1, 2, 3])
 
 
 @then('the X coordinate of each star is 800')
 def then_x3(context):
     """Then the X coordinate of each star is 800."""
-    for i in range(len(context.stars_list)):
-        assert(context.stars_list[i]['x'] == 800)
+    for i in range(len(context.stars_list['list'])):
+        assert(context.stars_list['list'][i]['x'] == 800)
 
 
 @then('the Y coordinate of each star is between 0 and 600')
 def then_y3(context):
     """Then the Y coordinate of each star is between 0 and 600."""
-    for i in range(len(context.stars_list)):
-        assert(context.stars_list[i]['y'] >= 0 and
-               context.stars_list[i]['y'] <= 600)
+    for i in range(len(context.stars_list['list'])):
+        assert(context.stars_list['list'][i]['y'] >= 0 and
+               context.stars_list['list'][i]['y'] <= 600)
 
 
 @then('the speed of each star is 1, 2 or 3')
 def then_v3(context):
     """Then the speed of each star is 1, 2 or 3."""
-    for i in range(len(context.stars_list)):
-        assert(context.stars_list[i]['v'] in [1, 2, 3])
+    for i in range(len(context.stars_list['list'])):
+        assert(context.stars_list['list'][i]['s'] in [1, 2, 3])
 
 
 # Scenario 4
@@ -118,11 +118,11 @@ def then_v3(context):
 @given('a list of objects representing stars')
 def given_stars_list(context):
     """Given a list of objects representing stars."""
-    context.stars_list = create_stars_list(20, x=800, y=[0, 600], v=[1, 2, 3])
+    context.stars_list = create_stars_list(20, x=800, y=[0, 600], s=[1, 2, 3])
 
 
 @when('I move the stars in the list')
-def when_move4(context):
+def when_move_stars_list(context):
     """When I move the stars in the list."""
     move_stars(context.stars_list)
 
@@ -130,37 +130,40 @@ def when_move4(context):
 @then('the X coordinate of each star changes according to its speed')
 def then_x4(context):
     """Then the X coordinate of each star changes according to its speed."""
-    for i in range(len(context.stars_list)):
-        assert(context.stars_list[i]['x'] == 800 + context.stars_list[i]['v'])
+    for i in range(len(context.stars_list['list'])):
+        assert(context.stars_list['list'][i]['x'] == 800 -
+               context.stars_list['list'][i]['s'])
 
 
 @then('the Y coordinate of each star does not change')
 def then_y4(context):
     """Then the Y coordinate of each star does not change."""
-    for i in range(len(context.stars_list)):
-        assert(context.stars_list[i]['y'] >= 0
-               and context.stars_list[i]['y'] <= 600)
+    for i in range(len(context.stars_list['list'])):
+        assert(context.stars_list['list'][i]['y'] >= 0
+               and context.stars_list['list'][i]['y'] <= 600)
 
 
 @then('the speed of each star does not change')
 def then_v4(context):
     """Then the speed of each star does not change."""
-    for i in range(len(context.stars_list)):
-        assert(context.stars_list[i]['v'] in [1, 2, 3])
+    for i in range(len(context.stars_list['list'])):
+        assert(context.stars_list['list'][i]['s'] in [1, 2, 3])
 
 
 # Scenario 5 - INCOMPLETE
 
-@given(u'a specific list of stars')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given a specific list of stars')
+@given('a specific list of stars')
+def given_stars_list2(context):
+    """Given a specific list of stars."""
+    # for row in context.table:
+    #    context.model.add_user(x=row['x'], y=row['y'], speed=row['speed'])
 
 
-@when(u'I move the stars 4 times')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When I move the stars 4 times')
+@when('I move the stars 4 times')
+def when_move_4times(context):
+    """When I move the stars 4 times."""
 
 
-@then(u'the list will have only 2 stars')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the list will have only 2 stars')
+@then('the list will have only 2 stars')
+def then_last_is_deleted(context):
+    """Then the list will have only 2 stars."""
--- aula-05/starfield.py
+++ aula-05/starfield.py
@@ -16,40 +16,63 @@ def create_star(**kwargs):
     else:
         y_range = kwargs['y']
         y = random.randint(y_range[0], y_range[1])
-
-    v = random.choice(kwargs['v'])
+    if type(kwargs['s']) == int:
+        s = kwargs['s']
+    else:
+        s = random.choice(kwargs['s'])
 
     # print("coord x = {}. Type: {}".format(x, type(x)))
     # print("coord y = {}. Type: {}".format(y, type(y)))
-    # print("vel v = {}. Type: {}".format(v, type(y)))
+    # print("speed s = {}. Type: {}".format(s, type(y)))
 
-    return {'x': x, 'y': y, 'v': v}
+    return {'x': x, 'y': y, 's': s}
 
 
 def move_star(star):
     """Move a star."""
-    star['x'] += star['v']
+    star['x'] -= star['s']
 
 
 def create_stars_list(quantity, **kwargs):
     """Create a list of stars."""
     index = 0
+    moves = 0
     stars_list = []
     while index < quantity:
         stars_list.append(create_star(**kwargs))
         index += 1
-    return stars_list
+    return {'list': stars_list, 'moves': moves}
 
 
 def move_stars(stars_list):
     """Move all the stars given a list of stars."""
-    for i in range(len(stars_list)):
-        move_star(stars_list[i])
+    stars_list['moves'] += 1
+    if stars_list['moves'] == 4:
+        stars_list['moves'] = 0
+        stars_list['list'].pop(0)
+    for i in range(len(stars_list['list'])):
+        move_star(stars_list['list'][i])
 
 
-# star1 = create_star(x=123, y=[12, 52], v=[1, 2, 3])
+# star1 = create_star(x=123, y=[12, 52], s=[1, 2, 3])
 # print(star1)
 # move_star(star1)
 # print(star1)
-stars_list1 = create_stars_list(20, x=800, y=[0, 600], v=[1, 2, 3])
-print(stars_list1)
+# stars_list1 = create_stars_list(20, x=800, y=[0, 600], s=[1, 2, 3])
+# print(stars_list1)
+# manual_stars_list2 = [create_star(x=10, y=10, s=3),
+#                       create_star(x=10, y=20, s=2),
+#                create_star(x=10, y=14, s=1)]
+# print(manual_stars_list2)
+# move_stars(manual_stars_list2)
+# print(manual_stars_list2)
+# move_stars(manual_stars_list2)
+# print(manual_stars_list2)
+# move_stars(manual_stars_list2)
+# print(manual_stars_list2)
+# move_stars(manual_stars_list2)
+# print(manual_stars_list2)
+stars_list1 = create_stars_list(3, x=800, y=[0, 600], s=[1, 2, 3])
+while len(stars_list1['list']) > 0:
+    print(stars_list1)
+    move_stars(stars_list1)

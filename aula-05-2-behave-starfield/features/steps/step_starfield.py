49cceea8dd7784af11f8f67531e1b6ac094249d2
Rodrigo Palermo <avilapalermo@gmail.com>
2018-10-03 18:25:51 -0300

    Substitued some constant values for variables in steps.

--- aula-05-2-behave-starfield/features/steps/step_starfield.py
+++ aula-05-2-behave-starfield/features/steps/step_starfield.py
@@ -10,10 +10,10 @@ def given1(context):
     context.range_int = (0, 600)
 
 
-@given('a list with values 1, 2 and 3')
-def given2(context):
+@given('a list with values {s1:d}, {s2:d} and {s3:d}')
+def given2(context, s1, s2, s3):
     """Given a list with values 1, 2 and 3."""
-    context.list = [1, 2, 3]
+    context.list = [s1, s2, s3]
 
 
 @when('I creat an object tha representas a star')
@@ -24,19 +24,19 @@ def when1(context):
     context.result = create_star(y_tuple, speedlist)
 
 
-@then('the X coord is 800')
-def then_x(context):
-    """Then the X coord is 800."""
-    waited = 800
+@then('the X coord is {x:d}')
+def then_x(context, x):
+    """Then the X coord is known."""
+    waited = x
     observed = context.result[0]
     assert waited == observed
 
 
-@then('the Y coord is between 0 and 600')
-def then_y(context):
-    """Then the Y coord is between 0 and 600."""
+@then('the Y coord is between {y_min:d} and {y_max:d}')
+def then_y(context, y_min, y_max):
+    """Then the Y coord is betweenknown interval."""
     observed = context.result[1]
-    assert (0 <= observed <= 600) is True
+    assert (y_min <= observed <= y_max) is True
 
 
 @then('the star speed is 1, 2 or 3')
